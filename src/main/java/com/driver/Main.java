93302947058d5089733da87b4ee69eaea74eaae5
Ankitbagga75 <100955035+Ankitbagga75@users.noreply.github.com>
2022-10-05 11:06:30 +0530

    Add files via upload

---
+++ src/main/java/com/driver/Main.java
@@ -0,0 +1,21 @@
+package com.driver;
+
+public class Main {
+    public static void main(String[] args) {
+        B b = new B();
+        System.out.println(b.meth());
+
+    }
+}
+
+class A {
+    public String meth() {
+        return "A";
+    }
+}
+
+class B extends A {
+    public String meth() {
+        return "B";
+    }
+}
